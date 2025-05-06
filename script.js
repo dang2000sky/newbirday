e3839f04c82619d2a92dca46bdfa84839a4f8720
dang2000sky <ngonguyenminhdang2010@gmail.com>
2025-05-06 17:40:52 +0000

    kkk

---
+++ script.js
@@ -0,0 +1,17 @@
+// Đợi DOM load xong mới chạy
+document.addEventListener("DOMContentLoaded", function () {
+    const stopButton = document.getElementById("stopBtn");
+    const star = document.querySelector(".gift__bow");
+    const gift = document.querySelector(".gift__lid");
+    const gift4 = document.querySelector(".gift__box");
+    const text = document.querySelector(".text");
+
+
+    // Khi nhấn nút "Dừng hiệu ứng"
+    stopButton.addEventListener("click", function() {
+        gift4.style.animation = "openhop 5s forwards";
+        star.style.animation = "no 1s forwards"; // Dừng hiệu ứng
+        gift.style.animation = "opennap 1s forwards"; // Dừng hiệu ứng
+        text.style.animation = "ease-in-out 5s xuathien forwards"; // Dừng hiệu ứng
+    });
+});
